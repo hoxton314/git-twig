8438a365e3fec5040384c0ebca0c9b0c4d933593
Igor Kalicinski <igorkalicinski@gmail.com>
2026-04-11 20:05:16 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
 const app = mount(App, {
   target: document.getElementById('app')!,
 })
